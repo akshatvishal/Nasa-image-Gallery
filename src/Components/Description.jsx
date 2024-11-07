f5d6860388b4493ab9cd9fddcd5d03be18b69a3c
akshat vishal <akshatvishal05@gmail.com>
2024-11-07 20:10:13 +0530

    error fixed

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 function Description() {
     const {nasa_id}=useParams();
-    const [item,setitem]=useState()
-  const fetch=async()=>{
+  const datafectch=async()=>{
     const res=await axios.get(`https://images-api.nasa.gov/search?nasa_id=${nasa_id}`)
-    console.log(res);
+    console.log(res.data);
 }
+datafectch()
   return (
     <div>
       
